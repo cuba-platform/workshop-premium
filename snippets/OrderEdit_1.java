1b5bd42f47c4a732ef131aa47554b4657ee37d31
Olga Shiryaeva <shiryaeva@haulmont.com>
2017-12-04 18:49:28 +0400

    ide screenshots

---
+++ snippets/OrderEdit_1.java
@@ -0,0 +1,9 @@
+public class OrderEdit extends AbstractEditor<Order> {
+
+    @Override
+    protected void initNewItem(Order item) {
+        super.initNewItem(item);
+
+        item.setStatus(OrderStatus.NEW);
+    }
+}
