3d14c52a9621938904514dd851aede0846b02bca
Olga Shiryaeva <shiryaeva@haulmont.com>
2017-12-04 13:12:54 +0400

    Added screenshots and snippets folders

---
+++ snippets/OrderService_1.java
@@ -0,0 +1,11 @@
+package com.company.workshop.service;
+
+import com.company.workshop.entity.Order;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public interface OrderService {
+    String NAME = "workshop_OrderService";
+
+    BigDecimal calculateAmount(Order order);
+}
