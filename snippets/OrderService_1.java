64a114e00132709865ff378dc9abe2b4e2fbea8d
Olga Shiryaeva <shiryaeva@haulmont.com>
2017-12-05 10:33:16 +0400

    services screenshots

--- snippets/OrderService_1.java
+++ snippets/OrderService_1.java
@@ -1,8 +1,8 @@
 package com.company.workshop.service;
 
-import com.company.workshop.entity.Order;
-import java.math.BigDecimal;
-import java.util.UUID;
+        import com.company.workshop.entity.Order;
+        import java.math.BigDecimal;
+        import java.util.UUID;
 
 public interface OrderService {
     String NAME = "workshop_OrderService";
